146e49ea1274135ca44c5bf5e428541ee5d36058
Ajay swarnkar <adhyatmavaani@gmail.com>
2026-05-30 16:29:56 +0530

    Clear legacy web app cache on React startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./styles/globals.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .catch(() => undefined);
+}
+
+if ("caches" in window) {
+  caches.keys()
+    .then((keys) => Promise.all(keys.filter((key) => key.toLowerCase().includes("flutter")).map((key) => caches.delete(key))))
+    .catch(() => undefined);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
